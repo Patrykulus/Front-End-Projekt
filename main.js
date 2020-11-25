9413935b2f7bbe5f33fff56c6e038a9b195b38ab
klaudiakindlik <klaudia.kindlik@gmail.com>
2020-11-25 12:45:56 +0100

    podstrony

---
+++ main.js
@@ -0,0 +1,12 @@
+const zdjecia = document.querySelectorAll(".intro-slideshow img");
+
+const nextImageDelay = 5000;
+let currentImageCounter = 0;
+slideshowImages[currentImageCounter].style.display = "block";
+
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+
+}
